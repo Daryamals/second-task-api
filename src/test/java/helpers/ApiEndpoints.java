cb4ce73e5168d90b49274ed7792f78c11608aaf6
omanytea <omanytea@school21.ru>
2025-04-07 13:07:04 +0300

    fix uri

--- src/test/java/helpers/ApiEndpoints.java
+++ src/test/java/helpers/ApiEndpoints.java
@@ -1,7 +1,7 @@
 package helpers;
 
 public class ApiEndpoints {
-	public static final String BASE_URI = "http://localhost:8080/";
+	public static final String BASE_URI = System.getProperty("base.url", "http://localhost:8080");
 	public static final String CREATE = "/api/create";
 	public static final String GET = "/api/get/{id}";
 	public static final String UPDATE = "/api/patch/{id}";
